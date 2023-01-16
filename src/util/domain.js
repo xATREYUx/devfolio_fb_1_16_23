4fef93a9776b8fd3ee25ff5b7af84523b074839a
xATREYUx <mwmailing@gmail.com>
2023-01-16 16:12:23 -0500

    first commit

--- src/util/domain.js
+++
@@ -1,5 +0,0 @@
-export default process.env.NODE_ENV === "development"
-  ? "http://localhost:3000"
-  : // "https://us-central1-devport-express-backend.cloudfunctions.net/app"
-    process.env.NODE_ENV === "production" && "https://folio-9-26-21.web.app";
-// "https://auth-skeleton-backend.herokuapp.com";
